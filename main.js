2f8cd1b9be7c91b6454fd7957cf1d849c2578ce7
Amm0n1te <103222407+Amm0n1te@users.noreply.github.com>
2023-06-01 17:11:50 -0700

    loading texture to cube

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import * as THREE from 'three';
 function main() {
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+
   const fov = 75;
   const aspect = 2;  // the canvas default
   const near = 0.1;
@@ -13,18 +14,41 @@ function main() {
 
   const scene = new THREE.Scene();
 
-  const boxWidth = 1;
-  const boxHeight = 1;
-  const boxDepth = 1;
-  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  const loader = new THREE.TextureLoader();
 
-  const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
-
-  const cube = new THREE.Mesh(geometry, material);
+  const sky_geom = new THREE.BoxGeometry(1,1,1);
+  const test_mat = new THREE.MeshBasicMaterial({
+    //color: 0x7d7d7d,
+    map: loader.load("resources/awkward_cat.jpg")
+  });
+  const sky_mats = [
+    test_mat,
+    test_mat,
+    test_mat,
+    test_mat,
+    test_mat,
+    test_mat,
+  ]
+  const cube = new THREE.Mesh(sky_geom, test_mat);
 
   scene.add(cube);
 
   renderer.render(scene, camera);
-}
+  
+  function render(time) {
+    time *= 0.001;  // convert time to seconds
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+  
+    renderer.render(scene, camera);
+  
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 
+
+
+
+} //main() brace
 main();
+
